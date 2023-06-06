255384b92ddd30495219e1cdee5d6d92a6669857
Francis Chioma <ichiomafrancis@gmail.com>
2023-06-06 22:38:21 +0100

    added a background color to the body and the app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import meme from './img/new_meme.svg';
 
 function App() {
   return (
-    <div className='body h-screen flex justify-center items-center'>
-      <div className='app w-[550px] border border-[#E5E5E5] rounded overflow-hidden'>
+    <div className='body h-screen flex justify-center items-center bg-[#E5E5E5]'>
+      <div className='app w-[550px] bg-white border border-[#D5D4D8] rounded-md overflow-hidden'>
         <div className=' header flex justify-between items-center h-16 bg-gradient-to-r from-dark-purple to-light-purple py-5 pl-5 pr-9 text-white shadow'>
           <div className='title flex'>
             <img src={troll} alt='' />
@@ -20,12 +20,12 @@ function App() {
             <input
               type='text'
               placeholder='top text'
-              className='w-56 p-2 border border-[#E5E5E5] outline-0 rounded-md'
+              className='w-56 p-2 border border-[#D5D4D8] outline-0 rounded-md'
             />
             <input
               type='text'
               placeholder='bottom text'
-              className='w-56 px-2 border border-[#E5E5E5] outline-0 rounded-md'
+              className='w-56 px-2 border border-[#D5D4D8] outline-0 rounded-md'
             />
           </div>
 
@@ -33,7 +33,7 @@ function App() {
             <img src={meme} alt='' className='' />
           </button>
 
-          <div className='mt-9 mb-14 w-full h-[300px] border border-[#E5E5E5]'></div>
+          <div className='mt-9 mb-14 w-full h-[300px] border border-[#D5D4D8]'></div>
         </div>
       </div>
     </div>
